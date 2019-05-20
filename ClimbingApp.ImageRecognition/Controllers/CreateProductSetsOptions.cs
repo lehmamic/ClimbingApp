7836570d989209fe5c288d179279e7e7f5150320
Michael Lehmann <lehmamic@bluewin.ch>
2019-05-20 21:35:42 +0200

    first commit

---
+++ ClimbingApp.ImageRecognition/Controllers/CreateProductSetsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ClimbingApp.ImageRecognition.Controllers
+{
+    internal class CreateProductSetsOptions
+    {
+        public string ProjectID { get; internal set; }
+        public string ComputeRegion { get; internal set; }
+        public string ProductSetId { get; internal set; }
+        public string ProductSetDisplayName { get; internal set; }
+    }
+}
