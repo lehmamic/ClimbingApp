c93b6d697a3c339b4341a4b7a0db77e490e94bbe
Michael Lehmann <lehmamic@bluewin.ch>
2019-06-13 22:05:13 +0200

    rename the query state

--- ClimbingAppIonic/src/app/query/query.reducer.spec.ts
+++ ClimbingAppIonic/src/app/query/query.reducer.spec.ts
@@ -1,13 +1,13 @@
-import { reducer, initialState } from './query.reducer';
+import { reducer, initialQueryState } from './query.reducer';
 
 describe('Query Reducer', () => {
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as any;
 
-      const result = reducer(initialState, action);
+      const result = reducer(initialQueryState, action);
 
-      expect(result).toBe(initialState);
+      expect(result).toBe(initialQueryState);
     });
   });
 });
