6d41201b196da2fa766eb27fd81375f562e800c3
Michael Lehmann <lehmamic@bluewin.ch>
2019-07-23 00:39:15 +0200

    Add some simple log to trigger the build

--- ClimbingApp.Routes/Controllers/Query/QueryController.cs
+++ ClimbingApp.Routes/Controllers/Query/QueryController.cs
@@ -5,6 +5,7 @@ using ClimbingApp.Routes.Controllers.ClimbingRoutes;
 using ClimbingApp.Routes.Entities;
 using ClimbingApp.Routes.Services.ImageRecognition;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Session;
 
@@ -17,12 +18,14 @@ namespace ClimbingApp.Routes.Controllers.Query
         private readonly IAsyncDocumentSession documentSession;
         private readonly IMapper mapper;
         private readonly IImageRecognitionApiClient imageRecognition;
+        private readonly ILogger<QueryController> logger;
 
-        public QueryController(IAsyncDocumentSession documentSession, IMapper mapper, IImageRecognitionApiClient imageRecognition)
+        public QueryController(IAsyncDocumentSession documentSession, IMapper mapper, IImageRecognitionApiClient imageRecognition, ILogger<QueryController> logger)
         {
             this.documentSession = documentSession ?? throw new System.ArgumentNullException(nameof(documentSession));
             this.mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
             this.imageRecognition = imageRecognition ?? throw new System.ArgumentNullException(nameof(imageRecognition));
+            this.logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
         }
 
         [HttpPost]
@@ -34,6 +37,8 @@ namespace ClimbingApp.Routes.Controllers.Query
             QueryResult match = response.Results.OrderByDescending(r => r.Score).FirstOrDefault(r => r.Score > 0.85);
             if (match != null)
             {
+                this.logger.LogInformation("Found a matching target");
+
                 match.Target.Labels.TryGetValue(ClimbingRoutesConstants.CLIMBING_ROUTE_ID_LABEL, out string climbingRouteId);
                 ClimbingSite site = await this.documentSession.Query<ClimbingSite>()
                     .FirstOrDefaultAsync(s => s.Routes.Any(r => r.Id == climbingRouteId));
